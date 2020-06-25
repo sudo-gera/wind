cea0e9f7229c0aeec8ece2f25490149aad8313ff
sudo-gera <gera.tatarinov.999@mail.com>
2020-06-26 00:40:26 +0300

    d

--- wind.py
+++ wind.py
@@ -3,12 +3,11 @@ from sys import argv
 from os import popen
 from os import system
 from os.path import abspath
-for w in range(2,len(argv)):
+for w in range(1,len(argv)):
  if '/' in argv[w]:
-#  argv[w]=abspath(argv[w])
   if system('wslpath -m '+argv[w]+'> /dev/null 2>/dev/null')==0:
    argv[w]=popen('wslpath -m '+argv[w]).read().strip()
 q=' '.join(argv[1:])
 print(q)
-system(q)
+system('"'+q+'"')
 print('\n')
