b3dca32c5934f3d04c0753ede4ba2332561c0f94
sudo-gera <gera.tatarinov.999@gmail.com>
2020-08-19 21:47:36 +0300

    j

--- install.py
+++ install.py
@@ -4,4 +4,5 @@ from os.path import *
 from sys import argv
 q=abspath(argv[0])
 q=q[:-len(q.split('/')[-1])]
-a=system('ln '+q+'wind.py ~/.local/bin/wind')
+from subprocess import run
+system(' '.join(['ln',q+'wind.py','~/.local/bin/wind']))
