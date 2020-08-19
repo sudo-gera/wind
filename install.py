4df6b64c383bef81205892706e8e26e52d174f2c
sudo-gera <gera.tatarinov.999@gmail.com>
2020-08-19 20:55:52 +0300

    /

--- install.py
+++ install.py
@@ -4,4 +4,4 @@ from os.path import *
 from sys import argv
 q=abspath(argv[0])
 q=q[:-len(q.split('/')[-1])]
-a=system('sudo ln '+q+'wind.py /usr/bin/wind')
+a=system('ln '+q+'wind.py ~/.local/bin/wind')
--- wind.py
+++ wind.py
@@ -3,9 +3,6 @@ python3 $0 $@
 exit
 ':'''
 from sys import argv
-arg0=argv[1]
-argv=[w if w[0]=='-'  else w if w[0] != '/' else w[5].upper()+':'+w[6:] if w.startswith('/mnt/') else '\\\\wsl$\\Ubuntu'+w for w in argv]
-if '/' not in arg0:
-	argv[0]=arg0
+argv=[w if w[0] != '/' else w[5].upper()+':'+w[6:] if w.startswith('/mnt/') else '\\\\wsl$\\Ubuntu'+w for w in argv]
 from subprocess import run
-run(argv)
+run(argv[1:])
