58848dd6c1f43e61173d22a5457e2e2535fab67b
sudo-gera <gera.tatarinov.999@gmail.com>
2020-10-20 15:21:36 +0300

    exe

--- wind.py
+++ wind.py
@@ -1,13 +1,12 @@
-''':'
-python3 $0 $@
-exit
-':'''
+#!/usr/bin/python3
 from sys import argv
 from os import getenv
 home=getenv('HOME')
 if home[-1]!='/':
 	home+='/'
-argv=[home+w[2:] if w.startswith('~/') else home if w=='~' else w] 
+exe=argv[1]
+argv=[home+w[2:] if w.startswith('~/') else home if w=='~' else w for w in argv] 
 argv=[w if w[0] != '/' else w[5].upper()+':'+w[6:] if w.startswith('/mnt/') else '\\\\wsl$\\Ubuntu'+w for w in argv]
+argv[1]=exe
 from subprocess import run
 run(argv[1:])
