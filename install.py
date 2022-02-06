67d491ee5ce5fead2c0b9ecc89e7ed2d48b3f66d
sudo-gera <gera.tatarinov.999@gmail.com>
2022-02-06 19:55:16 +0000

    spaces

--- install.py
+++ install.py
@@ -4,7 +4,7 @@ from os.path import *
 from sys import argv
 q = str(abspath(dirname(__file__)))+'/'
 from subprocess import run
-path='/usr/bin/wind'
+path = '/usr/bin/wind'
 if not exists(path):
 	run(['sudo','ln','-s',q+'wind.py',path]).returncode
 	run(['sudo','chmod','777',path])
